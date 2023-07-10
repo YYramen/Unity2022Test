f210247803dc6cf8177cf3f2dfe5e603b9c77f22
YUSUKE <yoshidayusuke1124@gmail.com>
2023-07-10 17:00:14 +0900

    簡易的なスポーンシステムの作成

--- Assets/Scripts/Sample Scripts/SpawnerSystem.cs
+++ Assets/Scripts/Sample Scripts/SpawnerSystem.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Burst;
 
-public class SpawnerSystem : MonoBehaviour
+[BurstCompile]
+public partial struct SpawnerSystem : ISystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnCreate(ref SystemState state) { }
+
+    public void OnDestroy(ref SystemState state) { }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
     {
-        
+        // すべてのSpawnerコンポーネントをクエリします。このシステムは、
+        // コンポーネントから読み取りと書き込みを行う必要があるため、RefRWを使用します。
+        // システムが読み取り専用のアクセスのみを必要とする場合は、RefROを使用します。
+        foreach (RefRW<Spawner> spawner in SystemAPI.Query<RefRW<Spawner>>())
+        {
+            ProcessSpawner(ref state, spawner);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ProcessSpawner(ref SystemState state, RefRW<Spawner> spawner)
     {
-        
+        // 次のスポーン時間が経過している場合    
+        if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
+        {
+            // スポナーの持つプレハブを使って、新しいエンティティを生成します。
+            Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
+            // LocalPosition.FromPositionは、指定された位置で初期化されたTransformを返します。
+            state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition));
+
+            // 次のスポーン時間をリセットします。
+            spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+        }
     }
 }
